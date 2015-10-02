26dc800199f63c484b41ab821ae74f1063389be0
Chris MacNaughton <chmacnaughton@gmail.com>
2015-10-02 09:23:33 -0400

    clear warnings
    fix bad merge

--- src/main.rs
+++ src/main.rs
@@ -425,7 +425,7 @@ fn main() {
             let mut cap = Capture::from_device(dev_device).unwrap() //open the device
                           .promisc(true)
                           //.snaplen(500)
-                          .timeout(50) 
+                          .timeout(50)
                           .open() //activate the handle
                           .unwrap(); //assume activation worked
             println!("Setting up filter");
@@ -446,11 +446,12 @@ fn main() {
                     let data = packet.data;
                     let mut cursor = Cursor::new(&data[..]);
                     let packet_header = parse_etherframe(&mut cursor);
+                    //Only try to parse Ceph Msgr packets.  The others don't really matter at the moment
                     let result = dissect_msgr(&mut cursor);
                     if result.is_ok(){
                         if packet_header.is_ok(){
                             let p = packet_header.unwrap();
-                            let print_result = process_packet(p, result.unwrap());
+                            let _ = process_packet(p, result.unwrap(), &args);
                         }
                     }
                 }
